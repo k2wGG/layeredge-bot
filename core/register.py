4c6e1e774efd7b2c4e84a08da2842de88ff003b3
Torint <103586560+k2wGG@users.noreply.github.com>
2025-02-22 21:58:56 +0300

    Update register.py

--- core/register.py
+++ core/register.py
@@ -4,7 +4,8 @@ from utils.private_key_to_wallet import private_key_to_wallet
 from random import choice
 from core.reqs import register_wallet
 from utils.total_time_to_register import get_random_delay
-
+from utils.move_account import move_account_to_farm  # Импорт функции перемещения
+from configs.constants import SUCCESS_PATH, FARM_PATH      # Пути для файлов: например, results/success.txt и configs/farm.txt
 import asyncio
 from core.farm import process_account
 
@@ -20,7 +21,14 @@ async def start():
         logger.info(f"{wallet_address} | Starting to register an account..")
         registered = await register_wallet(private_key, wallet_address, proxy, choice(REFS))
         if registered:
+            logger.success(f"{wallet_address} | Successfully register account")
+            # Перемещаем ключ из файла с успешными регистрациями в файл для фарма
+            move_account_to_farm(private_key, SUCCESS_PATH, FARM_PATH)
+            # Запускаем фарминг для аккаунта
             asyncio.create_task(process_account(private_key, proxy))
         if i != TOTAL_WALLETS - 1:
-            await asyncio.sleep(get_random_delay(TOTAL_WALLETS)/2)
+            await asyncio.sleep(get_random_delay(TOTAL_WALLETS) / 2)
     await asyncio.sleep(float("inf"))
+
+if __name__ == '__main__':
+    asyncio.run(start())
